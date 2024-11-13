5e502bda9eb49f0eedb8db8b6f8c878a83592ef1
Yuki Fujisawa <183579752+15bnl@users.noreply.github.com>
2024-11-13 19:14:42 +0000

    1114

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,21 @@ class BooksController < ApplicationController
   protect_from_forgery
   
   def new
+    @books = Book.all
     @book = Book.new
     # @books = Book.all
     # indexの記述必要？
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      # '/books/new'
+    else
+      @books = Book.all
+      render :new
+    end
   end
 
   def index
@@ -22,6 +28,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books/new'  # 投稿一覧画面へリダイレクト
   end
   
   private
